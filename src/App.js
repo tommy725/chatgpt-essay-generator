a29870f134d2aa06f273b5ac58650ef8292ad4db
RambarRambara <afteryou725@gmail.com>
2023-02-01 02:45:52 +0900

    Building UI.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Input, Typography, Card, Col, Row} from 'antd';
+import { useState } from 'react';
+const { Title, Text } = Typography;
+const { TextArea } = Input;
 
 function App() {
+  const [value, setValue] = useState('');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Title>ChatGPT Detector</Title>
+      <Row gutter={16}>
+        <Col span={12} offset={4}>
+          <Card className="no-padding">
+            <Row gutter={16}>
+              <Col span={16}>
+                  <Text strong>Enter an essay topic.</Text>
+                  <Input placeholder="" />
+                  <Text strong>Ant Design (strong)</Text>
+                  <TextArea placeholder="" autoSize />
+                  <div
+                    style={{
+                      margin: '24px 0',
+                    }}
+                  />
+              </Col>  
+              <Col span={8} className="">
+                
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 }
 
